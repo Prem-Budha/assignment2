bc756652db20cbeb44990ae891c6a9015c7624ed
Prem Budha <prem.budha9810@gmail.com>
2026-06-05 23:04:15 +0545

    master

--- src/project8.java
+++ src/project8.java
@@ -14,9 +14,12 @@ public class project8 {
     public static void main(String[] args){
         int num = 5;
         for (int i = 1 ; i <= 5; i++ ){
-            for (int j = 1 ; j <= 5; j++ ){
-                if
+            for (int j = 1 ; j <= i; j++ ){
+                System.out.print("* ");
+                System.out.print("  ");
+
             }
+            System.out.println(" ");
         }
     }
 }
